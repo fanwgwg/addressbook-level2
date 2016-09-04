923c851be6a9546d65733219990bf363fc413872
Fan Weiguang <lahalaha90@gmail.com>
2016-09-04 18:18:23 +0800

    Create interface Printable

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,12 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for all the printable info of a person in addressbook.
+ */
+public interface Printable {
+	
+	/**
+	 * @returna printable string representation of the object
+	 */
+	public String getPrintableString();
+}
