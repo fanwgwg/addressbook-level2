85280bf491498f9aeaec0aab4d1bfb113481adf0
Fan Weiguang <lahalaha90@gmail.com>
2016-09-07 14:50:38 +0800

    Add test case  for null

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -1,11 +1,28 @@
 package seedu.addressbook.data.person;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class NameTest {
+	
+	private Name name1;
+	
+	@Before
+	public void initialize() throws IllegalValueException {
+		name1 = new Name("John K Smith");
+	}
+	
 	@Test
 	public void testNothing() {
-		
+	}
+	
+	@Test
+	public void testOtherIsNull() {
+		assertFalse("Both are null", name1.isSimilar(null));
 	}
 
 }
